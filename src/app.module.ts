8270902332f89940cb910906e5e784cdc8f5b64f
hro-maker <hrant.muradyan.1998@mail.ru>
2021-04-08 14:58:20 +0400

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { MiddlewareConsumer, Module, RequestMethod } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { LoggerMiddleware } from "./track/middlwer/login.midleware";
 import { TrackModule } from "./track/track.module";
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 import { tracjControler } from './track/track.controller';
 @Module({
     imports:[
-    MongooseModule.forRoot('mongodb+srv://hro:hroos@cluster0.3l6j1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.3l6j1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`),
         TrackModule
     ]
 })
@@ -16,5 +18,6 @@ export class AppModule{
           .apply(LoggerMiddleware)
           .forRoutes(tracjControler);
       }
+      
 }
 // { path: 'hy', method: RequestMethod.GET }
